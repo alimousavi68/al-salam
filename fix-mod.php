d285bc22baf3fe574cdab7b1c52b4da55ea2d490
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2026-08-17 18:40:53 +0330

    chore: remove temporary development and test files

--- fix-mod.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once('../../../wp-load.php');
-remove_theme_mod('_alsalam_infra_mask');
-echo "Theme mod removed.";
--- run-customizer.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(dirname(dirname(__DIR__))) . '/wp-load.php';
-require_once __DIR__ . '/includes/seeder.php';
-
-alsalam_seed_customizer_options();
-echo "Done seeding customizer options\n";
